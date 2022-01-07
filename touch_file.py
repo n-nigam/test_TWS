2f9d30cb5317679bedb9dd2687d961c76286ec7c
n-nigam <96868780+n-nigam@users.noreply.github.com>
2022-01-08 00:50:08 +0530

    Changed output directory
    Created a separate directory "output" to write files into.

--- touch_file.py
+++ touch_file.py
@@ -5,7 +5,7 @@ import time
 timestr = time.strftime("%Y%m%d-%H%M%S")
 
 try:
-    with open('/home/ubuntu/TWS/test/' + timestr + '.txt', 'w') as f:
+    with open('/home/ubuntu/TWS/test/output' + timestr + '.txt', 'w') as f:
         f.write('Create a new test file!')
 except FileNotFoundError:
     print("The firectory dooes not exist")
